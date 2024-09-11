ac7da21688d42a15791aae92f1a4d358de540a8c
Felipe Castillo <felipecastillo.snk@gmail.com>
2024-09-11 16:01:59 -0300

    init project

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import cors from 'cors';
+import express from 'express';
+//import { PrismaClient } from '@prisma/client';
+
+const app = express();
+//const prisma = new PrismaClient();
+
+app.use(cors());
+app.use(express.json());
+
+// Rutas aqui
+app.get('/', (req, res) => {
+    res.send(
+        '<h1>Api Bodega</h1><br><p>Lorem Ipsum Merol</p>'
+    );
+});
+
+
+// Start Server
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Server escuchando en puerto ${PORT}`);
+});
